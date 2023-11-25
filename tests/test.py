a814fac31bc7fdf439d7dd8feb962727819b35d9
MikeInformatics <144392845+MikeInformatics@users.noreply.github.com>
2023-11-25 14:06:31 -0500

    Add files via upload

---
+++ tests/test.py
@@ -0,0 +1,35 @@
+
+# SDEV220-Software Development - Python
+
+# Name : M05 Programming Assignment - Testing
+# Author : M Mekoya 
+# Date : 11-25-2023 
+# Version: 1 
+# Purpose: Testing
+
+
+import unittest
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+
+        def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
