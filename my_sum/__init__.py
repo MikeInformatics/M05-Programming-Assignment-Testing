4672e094789100f922ad4a3e1ed0fb43bf6c7344
MikeInformatics <144392845+MikeInformatics@users.noreply.github.com>
2023-11-25 14:06:17 -0500

    Add files via upload

---
+++ my_sum/__init__.py
@@ -0,0 +1,17 @@
+# SDEV220-Software Development - Python
+
+# Name : M05 Programming Assignment - Testing
+# Author : M Mekoya 
+# Date : 11-25-2023 
+# Version: 1 
+# Purpose: Testing
+
+
+def sum(arg):
+    total = 0
+    for val in arg:
+        total += val
+    return total
+
+
+    
